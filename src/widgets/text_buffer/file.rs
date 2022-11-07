8617a1d9fae7ef1b2144f5e094133c3c8f1d0c6a
Raimundo Saona <37874270+saona-raimundo@users.noreply.github.com>
2022-11-07 09:37:47 +0100

    Fix "Sapce" typo (#21)

--- src/widgets/text_buffer/file.rs
+++ src/widgets/text_buffer/file.rs
@@ -72,7 +72,7 @@ impl Display for Indentation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Indentation::Tab(x) => write!(f, "Tab ({})", x),
-            Indentation::Space(x) => write!(f, "Sapce ({})", x),
+            Indentation::Space(x) => write!(f, "Space ({})", x),
         }
     }
 }
